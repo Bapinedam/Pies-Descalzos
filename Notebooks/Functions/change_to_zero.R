59a7c0024c03117039336901047e58ad537d36f7
Brayam Pineda <bapinedam@unal.edu.co>
2022-10-24 09:19:04 -0500

    Primera version final

---
+++ Notebooks/Functions/change_to_zero.R
@@ -0,0 +1,11 @@
+change_to_zero = function(x){
+  
+  # El objetivo de este función es transformar las puntuaciones negativas por cero
+  # ya que al efectuar algunas operaciones es posible obtener valores por debajo de cero.
+  
+  if(x < 0){
+    x=0
+  }
+  x
+
+}
---
+++ Notebooks/creacion_pdf.R
@@ -0,0 +1,8 @@
+rmarkdown::render(input="1.0-bapinedam-Primera_aplicacion.Rmd",
+                  output_file = "../Pdf/Resultados primera aplicacion.pdf")
+
+
+rmarkdown::render(input="2.0-bapinedam-Segunda_aplicacion.Rmd",
+                  output_file = "../Pdf/Resultados segunda aplicacion.pdf")
+
+
